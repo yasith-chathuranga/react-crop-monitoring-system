2babb5d1dc6f4e703a666cfdff9d1d40ffe5492d
yasith-chathuranga <yasithchathuranga999@gmail.com>
2025-01-19 21:43:26 +0530

    dashboard refactored

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,11 @@
+import {SearchBar} from "../../components/common/SearchBar.tsx";
+
 export function Dashboard() {
     return (
         <>
-
+            <div className="ml-[250px] p-5 transition-all ease-in-out duration-300 bg-background h-lvh">
+                <SearchBar/>
+            </div>
         </>
     );
 }
